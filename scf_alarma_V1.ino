e21b9501f88b20be82c72e293a7e7639190a72e2
Jorgetellofuentes <105795058+Jorgetellofuentes@users.noreply.github.com>
2022-05-26 12:21:17 +0200

    Add files via upload

--- scf_alarma_V1.ino
+++ scf_alarma_V1.ino
@@ -7,7 +7,18 @@ int led_5_amarillo = 13;// the PWM pin the LED is attached to
 
 
 int sensor_1= 2;
+int sensor_2= 3;
+int sensor_3= 4;
+int sensor_4= 5;
+int sensor_5= 6;
+
+
 int sensor_ventana_1;
+int sensor_ventana_2;
+int sensor_ventana_3;
+int sensor_ventana_4;
+int sensor_ventana_5;
+
 // the setup routine runs once when you press reset:
 void setup() 
 {
@@ -19,6 +30,11 @@ void setup()
   pinMode(led_5_amarillo, OUTPUT);
 
   pinMode(sensor_1, INPUT);
+  pinMode(sensor_2, INPUT);
+  pinMode(sensor_3, INPUT);
+  pinMode(sensor_4, INPUT);
+  pinMode(sensor_5, INPUT);
+  
   
   
 }
@@ -37,24 +53,54 @@ void loop()
   else
    
     digitalWrite(led_1_azul,0);
-
-
+  
   // set the pin of pin 10:
-  digitalWrite(led_2_verde,1);
+ sensor_ventana_2=digitalRead(sensor_2);
+  if (sensor_ventana_2==1)
+  {
+    digitalWrite(led_2_verde,1);
+
+  }
+  else
+   
+    digitalWrite(led_2_verde,0);
 
 
+    
   // set the pin of pin 11:
-  digitalWrite(led_3_rojo,1);
+   sensor_ventana_3=digitalRead(sensor_3);
+  if (sensor_ventana_3==1)
+  {
+    digitalWrite(led_3_rojo,1);
 
-  // set the pin of pin 12:
-  digitalWrite(led_4_blanco,1);
+  }
+  else
+   
+    digitalWrite(led_3_rojo,0);
+  
 
-  // set the pin 13 on:
-  digitalWrite(led_5_amarillo,1);
+  // set the pin of pin 121:
+   sensor_ventana_4=digitalRead(sensor_4);
+  if (sensor_ventana_4==1)
+  {
+    digitalWrite(led_4_blanco,1);
 
+  }
+  else
+   
+    digitalWrite(led_4_blanco,0);
+  
 
- 
+  // set the pin of pin 13:
+     sensor_ventana_5=digitalRead(sensor_5);
+  if (sensor_ventana_5==1)
+  {
+    digitalWrite(led_5_amarillo,1);
 
-
+  }
+  else
+   
+    digitalWrite(led_5_amarillo,0);
+  
 
 }
